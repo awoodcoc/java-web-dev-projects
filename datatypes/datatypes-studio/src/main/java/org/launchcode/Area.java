c3fdbedc071e3b6557eb1934cc4636fd68064515
Andrew <awoodcoc@indiana.edu>
2023-08-21 20:39:22 -0500

    Did the exercises and the studio.

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,13 +1,33 @@
 package org.launchcode;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius:");
+        boolean isValidNum = false;
         double radius = input.nextDouble();
-        double area = Circle.getArea(radius);
-        System.out.println("The area of that circle is " + area);
+        try {
+            while (!isValidNum)
+                if (radius < 0) {
+                    System.out.println("Enter a positive number for a radius!");
+                    radius= input.nextDouble();
+
+                }
+                else {
+                    isValidNum = true;
+                    Double areaCircle= Circle.getArea(radius);
+                    System.out.println("Area of the circle is: " + areaCircle );
+                }
+
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Not a valid input! Quitting.");
+        }
+
+
         input.close();
+
     }
 }
