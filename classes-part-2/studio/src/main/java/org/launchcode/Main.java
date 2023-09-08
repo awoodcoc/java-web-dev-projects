7f92371c4ba8251c1678c67f190e49554880b53f
Andrew <awoodcoc@indiana.edu>
2023-09-08 18:23:45 -0500

    Completed classes pt 2 studio

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,42 @@
 package org.launchcode;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+
+        //MAKING ITEMS
+        MenuItem item1 = new MenuItem("Pepperoni Pizza", "A pizza", 10.90, "Main course");
+        MenuItem item2 = new MenuItem("T Ravs", "Delicious ravs", 5.50, "Appetizer");
+        MenuItem item3 = new MenuItem("Gooey Butter Cake", "Delicious cake", 8.99, "Dessert");
+        MenuItem item4 = new MenuItem("Veggie Pizza", "OK za", 9.99, "Main course");
+        MenuItem item5 = new MenuItem("Garlic Bread", "awesome", 4.25, "Appetizer");
+        System.out.println(item1);
+        System.out.println(item2.isNew());
+
+        // CREATE MENU
+        Menu ourMenu = new Menu();
+        ourMenu.addMenuItem(item1);
+        ourMenu.addMenuItem(item2);
+        ourMenu.addMenuItem(item3);
+        ourMenu.addMenuItem(item5);
+        ourMenu.addMenuItem(item4);
+        System.out.println(ourMenu);
+
+        ourMenu.removeMenuItem(item1);
+        System.out.println(ourMenu);
+
+        // TEST EQUALS
+        System.out.println(item1.equals(item2));
+
+        MenuItem item6 = new MenuItem("Pepperoni Pizza", "Another kind of Za", 11.90, "Main course");
+
+        System.out.println(item1.equals(item6));
+
+
+        // BONUS MISSION
     }
 }
--- classes-part-2/studio/src/main/java/org/launchcode/Menu.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Menu.java
@@ -1,18 +1,19 @@
 package org.launchcode;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Menu {
-    private Date lastUpdated;
-    private ArrayList<MenuItem> items;
+    private LocalDate lastUpdated;
+    private ArrayList<MenuItem> items = new ArrayList<>();
 
-    public Menu(Date d, ArrayList<MenuItem> i) {
-        this.lastUpdated = d;
-        this.items = i;
-    }
+//    public Menu(LocalDate d, ArrayList<MenuItem> i) {
+//        this.lastUpdated = d;
+//        this.items = i;
+//    }
 
-    public void setLastUpdated(Date lastUpdated) {
+    public void setLastUpdated(LocalDate lastUpdated) {
         this.lastUpdated = lastUpdated;
     }
 
@@ -20,13 +21,62 @@ public class Menu {
         this.items = items;
     }
 
-    public Date getLastUpdated() {
+    public LocalDate getLastUpdated() {
         return lastUpdated;
     }
 
     public ArrayList<MenuItem> getItems() {
         return items;
     }
+
+    void addMenuItem(MenuItem newItem) {
+        String message = "This item is already on the menu";
+        if (items.contains(newItem)) {
+            System.out.println(message);
+            return;
+        }
+        for (MenuItem item : items) {
+            if (item.equals(newItem)) {
+                System.out.println(message);
+                return;
+            }
+        }
+
+        items.add(newItem);
+        lastUpdated = LocalDate.now();
+    }
+
+    void removeMenuItem(MenuItem item) {
+        items.remove(item);
+        lastUpdated = LocalDate.now();
+    }
+
+    // New override To String
+    @Override
+    public String toString() {
+        StringBuilder appetizers = new StringBuilder();
+        for (MenuItem item: items) {
+            if (item.getCategory().equals("Appetizer")) {
+                appetizers.append("\n").append(item).append("\n");
+            }
+        }
+        StringBuilder mainCourse = new StringBuilder();
+        for (MenuItem item: items) {
+            if (item.getCategory().equals("Main course")) {
+                mainCourse.append("\n").append(item).append("\n");
+            }
+        }
+        StringBuilder desserts = new StringBuilder();
+        for (MenuItem item: items) {
+            if (item.getCategory().equals("Dessert")) {
+                desserts.append("\n").append(item).append("\n");
+            }
+        }
+        return "\n Tony's Big Tasty Za\n" +
+                "Appetizers" + appetizers + "\n" +
+                "Main courses" + mainCourse + "\n" +
+                "Desserts" + desserts + "\n";
+    }
+
+
 }
-
-
